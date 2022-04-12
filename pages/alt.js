5a98bdb674bb39a793bf143c21bf16340711ac39
lofsigma <lofsigma@gmail.com>
2022-04-12 14:52:40 -0400

    new

--- pages/alt.js
+++ pages/alt.js
@@ -122,16 +122,17 @@ export default function Home() {
     // return (t) => toAngles(i(t));
   };
 
-  let projection = geoOrthographic()
+  const projection = geoOrthographic()
     .scale(250)
     .center([0, 0])
     .rotate([0, -30])
     .translate([width / 2, height / 2]);
 
   const [features, setFeatures] = useState([]);
+
   const [path, setPath] = useState(() => geoPath().projection(projection));
   const [scale, setScale] = useState(projection.scale());
-
+  const [angles, setAngles] = useState(projection.rotate());
   const graticule = geoGraticule();
 
   const fetchFeatures = () => {
@@ -176,17 +177,22 @@ export default function Home() {
   // projection.rotate(iv(t));
 
   // only runs once.
+  const dragBehavior = (e, d) => {
+    const rotate = projection.rotate();
+    const k = sens / projection.scale();
+    const rotation = [rotate[0] + e.dx * k, rotate[1] - e.dy * k];
+    projection.rotate(rotation);
+    setPath(() => geoPath().projection(projection));
+  };
+
+  const getCurrentAngles = () => {
+    console.log(angles);
+  };
+
   useEffect(() => {
     fetchFeatures();
     select(inputRef.current)
-      .call(
-        drag().on("drag", (e, d) => {
-          const rotate = projection.rotate();
-          const k = sens / projection.scale();
-          projection.rotate([rotate[0] + e.dx * k, rotate[1] - e.dy * k]);
-          setPath(() => geoPath().projection(projection));
-        })
-      )
+      .call(drag().on("drag", dragBehavior))
       .call(
         zoom().on("zoom", (e, d) => {
           if (e.transform.k > 0.3) {
@@ -206,17 +212,6 @@ export default function Home() {
     //   projection.rotate([rotate[0] - 1 * k, rotate[1]]);
     //   setPath(() => geoPath().projection(projection));
     // }, 200);
-    // let p1 = d3.geoCentroid(country);
-    // let r1 = [(-p2[0], 20 - p2[1], 0)];
-    // returns an interpolation function that goes from point one to point 2
-    // const ip = d3.geoInterpolate(p1, p2);
-
-    // returns an interpolation function that goes from point one to point 2.
-    // const iv = Versor.interpolateAngles(r1, r2);
-    // projection.rotate(iv(t));
-    // console.log("h1", fromAngles.interpolate);
-    // console.log("h2", fromAngles);
-    console.log("h2", interpolateAngles);
   }, []);
 
   return (
@@ -244,44 +239,13 @@ export default function Home() {
         </g>
         <g>
           <path
-            d={path(geoGraticule())}
+            d={path(graticule())}
             fill="none"
             stroke="black"
             strokeWidth="0.3"
             opacity="0.8"
           />
           {bBox()}
-          {/* {features.length && (
-            <path
-              fill="none"
-              stroke="red"
-              strokeWidth="1"
-              opacity="0.8"
-              strokeDasharray="5 5"
-              d={path(graticule.extentMajor(geoBounds(features[5])).outline())}
-            />
-          )}
-          {console.log(
-            geoGraticule().extentMajor(geoBounds(features[5])).outline()
-          )}
-          {console.log("a", path)}
-          {console.log("b", geoGraticule())}
-          {console.log("c", geoGraticule().extentMajor())}
-          {console.log("d", graticule.extentMajor(geoBounds(features[5])))}
-          {console.log(
-            "e",
-            JSON.stringify(
-              graticule.extentMajor(geoBounds(features[5])).outline()
-            )
-          )}
-          {console.log("f", geoBounds(features[5]))}
-          {console.log("g", features)}
-          {console.log(
-            "h",
-            path(graticule.extentMajor(geoBounds(features[5])).outline()).catch(
-              (e) => console.log(e)
-            )
-          )} */}
         </g>
         <path
           fill="none"
@@ -303,67 +267,27 @@ export default function Home() {
             .duration(1000)
             .tween("rot", () => (t) => {
               projection.rotate(
-                Versor.interpolateAngles(
-                  [0, 0, 0],
-                  [
-                    -geoCentroid(
+                Versor.interpolateAngles(angles, [
+                  -geoCentroid(
+                    features.filter((f) => f.properties.name === "Colombia")[0]
+                  )[0],
+                  10 -
+                    geoCentroid(
                       features.filter(
                         (f) => f.properties.name === "Colombia"
                       )[0]
-                    )[0],
-                    10 -
-                      geoCentroid(
-                        features.filter(
-                          (f) => f.properties.name === "Colombia"
-                        )[0]
-                      )[1],
-                    0,
-                  ]
-                )(t)
+                    )[1],
+                  0,
+                ])(t)
               );
               setPath(() => geoPath().projection(projection));
             });
-
-          // console.log(
-          //   fromAngles.interpolate(
-          //     [0, 0, 0],
-          //     [
-          //       -geoCentroid(
-          //         features.filter((f) => f.properties.name === "Colombia")[0]
-          //       )[0],
-          //       10 -
-          //         geoCentroid(
-          //           features.filter((f) => f.properties.name === "Colombia")[0]
-          //         )[1],
-          //       0,
-          //     ]
-          //   )(1)
-          // );
-
-          //   .projection.rotate([
-          //     -geoCentroid(
-          //       features.filter((f) => f.properties.name === "Colombia")[0]
-          //     )[0],
-          //     10 -
-          //       geoCentroid(
-          //         features.filter((f) => f.properties.name === "Colombia")[0]
-          //       )[1],
-          //     0,
-          //   ]);
-          // console.log([
-          //   -features.filter((f) => f.properties.name === "Colombia")[0],
-          //   10 - features.filter((f) => f.properties.name === "Colombia")[0][1],
-          //   0,
-          // ]);
-          // console.log(
-          //   features.filter((f) => f.properties.name === "Colombia")[0]
-          // );
-          setPath(() => geoPath().projection(projection));
         }}
       >
         flower
         {/* {console.log(interpolateAngles)} */}
       </button>
+      <button onClick={getCurrentAngles}>current Angle</button>
     </div>
   );
 }
