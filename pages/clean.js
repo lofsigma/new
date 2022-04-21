abe51e804385ce9b246f8282f1b82ce0dfe0bfa1
lofsigma <lofsigma@gmail.com>
2022-04-21 18:28:17 -0400

    hello versors!

--- pages/clean.js
+++ pages/clean.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect, useRef } from "react";
 import { drag } from "d3-drag";
 import { zoom, zoomIdentity } from "d3-zoom";
 import { feature } from "topojson-client";
-import { event, select } from "d3-selection";
+import { event, select, pointers } from "d3-selection";
 import { transition } from "d3-transition";
 import { format } from "d3-format";
 import { geoScaleBar } from "d3-geo-scale-bar";
+import { mean } from "d3-array";
 import { timer } from "d3-timer";
 // import geoZoom from "d3-geo-zoom";
 // import d3GeoZoom from "d3-geo-zoom";
@@ -37,68 +38,12 @@ export default function Home() {
       .translate([width / 2, height / 2])
   );
 
-  const zoomStarted = () => {
-    v0 = Versor.r0 = q0 = Versor.fromAngles(projection.rotate());
-  };
-
-  const zoomed = (e) => {
-    projection.scale(e.transform.k);
-    const v1 = Versor.cartesian(projRef.current.invert([e.dx, e.dy]));
-    const delta = versor.delta(v0, v1);
-
-    (q1 = versor.multiply(q0, versor.delta(v0, v1))),
-      (r1 = versor.rotation(q1));
-    projection.rotate(r1);
-
-    render();
-  };
-
-  function drag(projection) {
-    let v0, q0, r0;
-
-    function dragstarted(e) {
-      v0 = versor.cartesian(projection.invert([e.x, e.y]));
-      q0 = versor((r0 = projection.rotate()));
-    }
-
-    function dragged(e) {
-      const v1 = versor.cartesian(projection.rotate(r0).invert([e.x, e.y]));
-      const q1 = versor.multiply(q0, versor.delta(v0, v1));
-      projection.rotate(versor.rotation(q1));
-    }
-
-    return drag().on("start", dragstarted).on("drag", dragged);
-  }
+  // let drag = (proj) => {};
 
   const [features, setFeatures] = useState([]);
   const [path, setPath] = useState(() => geoPath().projection(projRef.current));
   const [active, setActive] = useState(null);
   const graticule = geoGraticule();
-
-  let zoomBehavior = zoom()
-    .scaleExtent([330, 10000])
-    .translateExtent([
-      [0, 0],
-      [width, height],
-    ])
-    .on("zoom", (e) => {
-      projRef.current.scale(e.transform.k);
-      setPath(() => geoPath().projection(projRef.current));
-
-      scaleBarGenerator.zoomFactor(e.transform.k);
-      select(document.getElementById("scale-bar-wrapper")).call(
-        scaleBarGenerator
-      );
-    });
-
-  let dragBehavior = drag().on("drag", (e) => {
-    const k = sens / projRef.current.scale();
-    projRef.current.rotate([
-      projRef.current.rotate()[0] + e.dx * k,
-      projRef.current.rotate()[1] - e.dy * k,
-    ]);
-    setPath(() => geoPath().projection(projRef.current));
-  });
 
   // scale bar generator
   const scaleBarGenerator = geoScaleBar()
@@ -117,9 +62,137 @@ export default function Home() {
       });
   };
 
+  let v0, q0, r0;
+
   useEffect(() => {
     // Fetch features from JSON file.
     fetchFeatures();
+
+    function dragBehavior(projection) {
+      let v0, q0, r0, a0, l;
+
+      function pointer(event, that) {
+        const t = pointers(event, that);
+
+        if (t.length !== l) {
+          l = t.length;
+          if (l > 1) a0 = Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]);
+          dragstarted.apply(that, [event, that]);
+        }
+
+        // For multitouch, average positions and compute rotation.
+        if (l > 1) {
+          const x = mean(t, (p) => p[0]);
+          const y = mean(t, (p) => p[1]);
+          const a = Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]);
+          return [x, y, a];
+        }
+
+        return t[0];
+      }
+
+      function dragstarted(event) {
+        v0 = versor.cartesian(projection.invert(pointer(event, this)));
+        q0 = versor((r0 = projection.rotate()));
+      }
+
+      function dragged(event) {
+        const p = pointer(event, this);
+        const v1 = versor.cartesian(projection.rotate(r0).invert(p));
+        const delta = versor.delta(v0, v1);
+        let q1 = versor.multiply(q0, delta);
+
+        // For multitouch, compose with a rotation around the axis.
+        if (p[2]) {
+          const d = (p[2] - a0) / 2;
+          const s = -Math.sin(d);
+          const c = Math.sign(Math.cos(d));
+          q1 = versor.multiply([Math.sqrt(1 - s * s), 0, 0, c * s], q1);
+        }
+
+        projection.rotate(versor.rotation(q1));
+
+        // In vicinity of the antipode (unstable) of q0, restart.
+        if (delta[0] < 0.7) dragstarted.apply(this, [event, this]);
+      }
+
+      return drag().on("start", dragstarted).on("drag", dragged);
+    }
+    function zoomBehavior(
+      projection,
+      {
+        // Capture the projection’s original scale, before any zooming.
+        scale = projection._scale === undefined
+          ? (projection._scale = projection.scale())
+          : projection._scale,
+        scaleExtent = [0.8, 8],
+      } = {}
+    ) {
+      let v0, q0, r0, a0, tl;
+
+      const zoom3 = zoom()
+        .scaleExtent(scaleExtent.map((x) => x * scale))
+        .on("start", zoomstarted)
+        .on("zoom", zoomed);
+
+      function point(event, that) {
+        const t = pointers(event, that);
+
+        if (t.length !== tl) {
+          tl = t.length;
+          if (tl > 1) a0 = Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]);
+          zoomstarted.call(that, event);
+        }
+
+        return tl > 1
+          ? [
+              d3.mean(t, (p) => p[0]),
+              d3.mean(t, (p) => p[1]),
+              Math.atan2(t[1][1] - t[0][1], t[1][0] - t[0][0]),
+            ]
+          : t[0];
+      }
+
+      function zoomstarted(event) {
+        v0 = versor.cartesian(projection.invert(point(event, this)));
+        q0 = versor((r0 = projection.rotate()));
+      }
+
+      function zoomed(event) {
+        projection.scale(event.transform.k);
+        const pt = point(event, this);
+        const v1 = versor.cartesian(projection.rotate(r0).invert(pt));
+        const delta = versor.delta(v0, v1);
+        let q1 = versor.multiply(q0, delta);
+
+        // For multitouch, compose with a rotation around the axis.
+        if (pt[2]) {
+          const d = (pt[2] - a0) / 2;
+          const s = -Math.sin(d);
+          const c = Math.sign(Math.cos(d));
+          q1 = versor.multiply([Math.sqrt(1 - s * s), 0, 0, c * s], q1);
+        }
+
+        projection.rotate(versor.rotation(q1));
+
+        // In vicinity of the antipode (unstable) of q0, restart.
+        if (delta[0] < 0.7) zoomstarted.call(this, event);
+      }
+
+      return Object.assign(
+        (selection) =>
+          selection
+            .property("__zoom", zoomIdentity.scale(projection.scale()))
+            .call(zoom3),
+        {
+          on(type, ...options) {
+            return options.length
+              ? (zoom3.on(type, ...options), this)
+              : zoom3.on(type);
+          },
+        }
+      );
+    }
 
     // initializes bar.
     // select(document.getElementById("scale-bar-wrapper")).call(
@@ -127,7 +200,18 @@ export default function Home() {
     // );
 
     // select(inputRef.current).call(dragBehavior).call(zoomBehavior);
-    select(inputRef.current).call(drag(projRef.current));
+    select(inputRef.current)
+      .call(
+        dragBehavior(projRef.current).on("drag.render", () =>
+          setPath(() => geoPath().projection(projRef.current))
+        )
+      )
+      .call(
+        zoomBehavior(projRef.current).on("zoom.render", () =>
+          setPath(() => geoPath().projection(projRef.current))
+        )
+      );
+    // .on("end.render", () => render(land50)));
     // geoZoom()
     //   .projection(projRef.current)
     //   .onMove(({ scale, rotation }) => {
