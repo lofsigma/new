32ebdb2f1468c5b29db3134937f3fd4e9ee0b4b2
lofsigma <lofsigma@gmail.com>
2022-05-17 12:37:25 -0400

    new zoom try in mobile

--- pages/clean4.js
+++ pages/clean4.js
@@ -1,16 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
-import { drag } from "d3-drag";
-import { zoom, zoomIdentity } from "d3-zoom";
-import { feature } from "topojson-client";
-import { event, select, pointers } from "d3-selection";
-import { transition } from "d3-transition";
-import { format } from "d3-format";
-import { geoScaleBar } from "d3-geo-scale-bar";
-import { mean } from "d3-array";
-import { timer } from "d3-timer";
-import { interpolateNumber } from "d3-interpolate";
-import { Delaunay } from "d3-delaunay";
-import { geoZoom } from "d3-geo-zoom";
+import { useState, useEffect, useRef } from "react";
+
 import {
   geoOrthographic,
   geoPath,
@@ -20,15 +9,16 @@ import {
   geoGraticule,
 } from "d3-geo";
 
-import Versor from "../lib/Versor";
-import versor from "../lib/oldVersor";
+import { geoZoom } from "../lib/index.js";
+import { feature } from "topojson-client";
 
 export default function Home() {
+  // Constants
   const width = 700;
   const height = 700;
 
-  const ref = useRef();
-  const inputRef = useRef();
+  // References
+  const svgRef = useRef();
   const projRef = useRef(
     geoOrthographic()
       .scale(250)
@@ -37,69 +27,42 @@ export default function Home() {
       .translate([width / 2, height / 2])
   );
 
+  // Hooks
   const [features, setFeatures] = useState([]);
   const [path, setPath] = useState(() => geoPath().projection(projRef.current));
-  const [active, setActive] = useState(null);
 
-  const fetchFeatures = () => {
+  // Fetch Features
+  const fetchFeatures = async () => {
     fetch("/countries-110m.json")
       .then((res) => res.json())
-      .then((d) => {
-        setFeatures(feature(d, d.objects.countries).features);
-      });
+      .then((d) => setFeatures(feature(d, d.objects.countries).features));
   };
 
+  const render = () => {
+    setPath(() => geoPath().projection(projRef.current));
+  };
+
+  // RUNS ONCE
   useEffect(() => {
-    select(inputRef.current);
-    //   // .call(
-    //   //   dragBehavior(projRef.current).on("drag.render", () =>
-    //   //     setPath(() => geoPath().projection(projRef.current))
-    //   //   )
-    //   // )
-    //   .call(
-    //     zoomBehavior(projRef.current).on("zoom.render", () =>
-    //       setPath(() => geoPath().projection(projRef.current))
-    //     )
-    // );
+    fetchFeatures();
 
-    geoZoom()
-      .projection(projRef.current)
-      .onMove(() => {
-        setPath(() => geoPath().projection(projRef.current));
-      })(inputRef);
+    geoZoom().projection(projRef.current).onMove(render)(svgRef.current);
   }, []);
-
   return (
-    <div className="wrapper">
-      <svg
-        width="100%"
-        height={height}
-        ref={inputRef}
-        viewBox="0 0 700 700"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <circle
-          fill="#d3d3d3"
-          stroke="000"
-          strokeWidth="0.2"
-          cx="50%"
-          cy="50%"
-          r={projRef.current.scale()}
-        />
+    <div
+      className="wrapper"
+      viewBox="0 0 700 700"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <svg ref={svgRef} width={width} height={height}>
         <g className="countries">
           {features.map((f) => (
             <path
               d={path(f)}
-              fill={f.properties.name === active ? "orange" : "white"}
               stroke="black"
               strokeWidth="0.3"
               opacity="0.8"
               key={f.properties.id}
-              onClick={() => {
-                f.properties.name === active
-                  ? setActive(null)
-                  : setActive(f.properties.name);
-              }}
             />
           ))}
         </g>
